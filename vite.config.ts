47bff2a0b98d72caf92482c3d1f863f0554337e5
Sriram Adithya <sriramadithya1999@gmail.com>
2024-03-28 21:40:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
     build: {
-      base: '/',
       rollupOptions: {
         external: [
           '/react-nyTimes/src/assets/The_New_York_Times_logo.png',
