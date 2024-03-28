23a498dd252ed6099b18b8ffcc251e8f280f8b1e
Sriram Adithya <sriramadithya1999@gmail.com>
2024-03-28 21:05:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,15 @@ export default defineConfig({
   plugins: [react()],
     build: {
       base : '/public_html/build/',
-      // rollupOptions: {
-      //   external: [
-      //     '/react-nyTimes/src/assets/The_New_York_Times_logo.png',
-      //     "/react-nyTimes/src/assets/image.png",
-      //     "/react-nyTimes/src/assets/games.png",
-      //     "/react-nyTimes/src/assets/digital.png",
-      //     "/react-nyTimes/src/assets/cooking.png",
-      //     '/^node:.*/',
-      //   ]
-      // }
+      rollupOptions: {
+        external: [
+          '/react-nyTimes/src/assets/The_New_York_Times_logo.png',
+          "/react-nyTimes/src/assets/image.png",
+          "/react-nyTimes/src/assets/games.png",
+          "/react-nyTimes/src/assets/digital.png",
+          "/react-nyTimes/src/assets/cooking.png",
+          '/^node:.*/',
+        ]
+      }
   }
 })
